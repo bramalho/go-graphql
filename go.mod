db1d142bf900cb1f6245c978a42949befb7849a8
Bruno Ramalho <bramalho@divante.pl>
2020-07-31 15:14:19 +0200

    api

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
-module github.com/bramalho/go-graphql
+module go-graphql
 
 go 1.14
+
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/graphql-go/graphql v0.7.9
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.4.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"go-graphql/handlers"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("It works")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handlers.Info).Methods("GET")
+	r.HandleFunc("/graphql", handlers.GraphQL)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	log.Println("Listning at http://localhost:" + port)
+	err := http.ListenAndServe(":"+port, r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
